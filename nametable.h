018c00e8203018f07ad25b432ca8295b34c22184
Simeon Simeonov <sss41@cam.ac.uk>
2011-05-20 00:09:06 +0100

    Add: comment explanation for the map data structure

--- nametable.h
+++ nametable.h
@@ -18,9 +18,15 @@ class Compare
   class NameTable
   {
   private:
+    /* Reason I did this instead of map<int, Name> is because with int
+     * as key to lookup a namestring you have to iterate through the whole
+     * table and compare values. This will have at least a linear O(N) cost
+     * whereas a map<Name, int> has a O(log(N)) cost since map is implemented
+     * as a BST.
+     * Also the Name type should make it easier to handle and work with names.
+     */
     std::map<Name, Value,Compare> nameTable;
   public:
-    //NameTable ();
     void InsertName (Name name, Value val = 0);
     void InsertName (namestring str, Value val = 0);
     void RemoveName (Name name);
